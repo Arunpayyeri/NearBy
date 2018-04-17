7f2a51c5b44d85d4c72892046fa73888c9cf85cf
arunpayyeri <arunpayyeri@gmail.com>
2018-04-17 15:43:48 +0530

    First commit

---
+++ app/src/main/java/com/example/arun/searchloc/Geometry.java
@@ -0,0 +1,28 @@
+package com.example.arun.searchloc;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Geometry {
+    @SerializedName("location")
+    @Expose
+    private Location location;
+
+    /**
+     *
+     * @return
+     * The location
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     *
+     * @param location
+     * The location
+     */
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
